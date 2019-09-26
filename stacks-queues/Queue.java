e6e017b5fbe441e3df151b6bf62ff9fd8a0d584b
lukban90 <lukban4c@gmail.com>
2019-09-25 22:56:13 -0700

    Stacks & Queues

---
+++ stacks-queues/Queue.java
@@ -0,0 +1,64 @@
+import java.util.LinkedList;
+import java.util.AbstractCollection;
+import java.util.Stack;
+
+public class Queue {
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        private Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int peek() {
+        return head.data;
+    }
+
+    public void add(int data) {
+        Node node = new Node(data);
+        if (tail != null)
+            tail.next = node;
+        tail = node;
+        if (head == null)
+            head = node;
+    }
+
+    public int remove() {
+        int data = head.data;
+        head = head.next;
+        if (head == null)
+            tail = null;
+        return data;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        Stack<Integer> s = new Stack<Integer>();
+
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(3);
+        l.add(4);
+
+        System.out.println(l);
+
+        while (!l.isEmpty()) {
+            s.push(l.poll());
+        }
+
+        while (!s.empty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
---
+++ stacks-queues/Stack.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Stack {
+    private static class Node {
+        private int data;
+        private Node next;
+
+        private Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node top;
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int peek() {
+        return top.data;
+    }
+
+    public void push(int data) {
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    }
+
+    public int pop() {
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        // Stack s = null;
+        Stack<Integer> s = new Stack<Integer>();
+
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+
+        System.out.println("Linked list: " + l);
+
+        // while (l != null) {
+        // try {
+        // s.push(l.poll());
+        // s.peek();
+        // } catch (NullPointerException e) {
+        // System.out.println(e);
+        // }
+        // }
+
+        // while (!s.isEmpty()) {
+        // System.out.println(s.pop());
+        // }
+    }
+}
